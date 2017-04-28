5a1e40ba880fae5f133dcb1563e87c060c5cf23e
Maxim Denisov <denisovmax1988@gmail.com>
2017-04-28 23:48:15 +0300

    Fix 'Accessing PropTypes via the main React package is deprecated.'

--- modules/ReduxAsyncConnect.js
+++ modules/ReduxAsyncConnect.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RouterContext from 'react-router/lib/RouterContext';
 import { beginGlobalLoad, endGlobalLoad } from './asyncConnect';
 import { connect } from 'react-redux';
 
-const { array, func, object, any } = React.PropTypes;
+const { array, func, object, any } = PropTypes;
 
 /**
  * We need to iterate over all components for specified routes.
