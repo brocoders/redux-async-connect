4f22cb1e692fd66fe3bd23fa2de2dc6eebc13830
Max Nikitin <maxnik4u@gmail.com>
2017-05-15 18:44:17 +0300

    Do not call promise if filtered

--- modules/ReduxAsyncConnect.js
+++ modules/ReduxAsyncConnect.js
@@ -42,9 +42,8 @@ function loadAsyncConnect({components, filter = () => true, ...rest}) {
     const asyncItems = Component.reduxAsyncConnect;
 
     return Promise.all(asyncItems.reduce((itemsResults, item) => {
-      let promiseOrResult = item.promise(rest);
-
       if (filter(item, Component)) {
+        let promiseOrResult = item.promise(rest);
         if (promiseOrResult && promiseOrResult.then instanceof Function) {
           async = true;
           promiseOrResult = promiseOrResult.catch(error => ({error}));
